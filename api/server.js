794645b13d3c3c551abf402d90e74df69f16f7ac
Bruno Santos <brunodsp.dev@gmail.com>
2026-05-18 18:16:39 -0300

    fix: correct json-graphql-server usage

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { createServer } = require("json-graphql-server");
+const { jsonGraphqlExpress } = require("json-graphql-server");
 const data = require("./db");
 
 const app = express();
 
-app.use("/graphql", createServer(data));
+app.use("/graphql", jsonGraphqlExpress(data));
 
 const PORT = process.env.PORT || 10000;
 
