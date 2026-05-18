3f8af3d6c64aa7ec2c2aace0118e816e26ea61f6
Bruno Santos <brunodsp.dev@gmail.com>
2026-05-18 20:23:26 -0300

    fix: corrigir servidor GraphQL com json-graphql-server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { jsonGraphqlExpress } = require("json-graphql-server");
+
+const jsonGraphqlServer =
+  require("json-graphql-server").default || require("json-graphql-server");
+
 const data = require("./db");
 
 const app = express();
 
-app.use("/graphql", jsonGraphqlExpress(data));
+app.use("/graphql", jsonGraphqlServer(data));
 
 const PORT = process.env.PORT || 10000;
 
